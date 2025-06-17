c197eee76b5ec485ad2bf5a425ea663e169ec665
Daniel Muhia <muhiadan17@gmail.com>
2025-06-17 22:20:23 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+
+function submitData(name, email){
+
+  return fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+      name,
+      email
+      }),
+    })
+    .then(response =>  response.json())
+    .then(responseData => {
+      const idElement = document.createElement('div');
+      idElement.textContent = responseData.id;
+
+    
+      document.body.appendChild(idElement);
+    })
+    .catch(error => {
+      const errorElement = document.createElement('div');
+      errorElement.textContent = error.message;
+
+      document.body.appendChild(errorElement);
+    });
+}
+
